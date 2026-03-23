667882e75499b5b7fed516d85ce7586553215ee3
Егорчик <153913263+Egorcheek@users.noreply.github.com>
2026-03-23 23:44:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const btn = document.getElementById("playBtn");
+const music = document.getElementById("bgMusic");
 
+let playing = false;
+
+btn.addEventListener("click", () => {
+    if (!playing) {
+        music.play();
+        btn.textContent = "⏸";
+    } else {
+        music.pause();
+        btn.textContent = "▶";
+    }
+    playing = !playing;
+});
