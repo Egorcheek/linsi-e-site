54fee9fd54a85cb8afb14be83c37e089053cdc81
Егорчик <153913263+Egorcheek@users.noreply.github.com>
2026-03-24 15:13:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const btn = document.getElementById("playBtn");
-const music = document.getElementById("bgMusic");
+fetch('posts.json')
+  .then(res => res.json())
+  .then(posts => {
+    const container = document.querySelector('.posts');
 
-btn.addEventListener("click", () => {
-    if (music.paused) {
-        music.play()
-            .then(() => {
-                btn.textContent = "⏸";
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    } else {
-        music.pause();
-        btn.textContent = "▶";
-    }
-});
+    posts.reverse().forEach(post => {
+      const el = document.createElement('div');
+      el.className = 'post';
+
+      el.innerHTML = `
+        <div class="post-date">${post.date}</div>
+        <div class="post-title">${post.title}</div>
+        <p>${post.text}</p>
+      `;
+
+      container.appendChild(el);
+    });
+  });
